f653b30ba7d6c24133060f045d6c0857ad192062
Bhoomi Chowksey <149859840+bhoomichowksey@users.noreply.github.com>
2026-07-18 15:10:20 +0530

    Delete frontend directory

--- frontend/src/components/layout/FloatingFeedback.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-
-export default function FloatingFeedback() {
-  return (
-    <Link to="/feedback" className="floating-feedback" title="Share your feedback">
-      Feedback
-    </Link>
-  );
-}
